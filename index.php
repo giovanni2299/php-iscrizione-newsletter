02d776aa5e684d5e8ec30ddfb4e9bd97e5215f5c
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-07 15:34:00 +0200

    ADD: bootstrap and decorate the document

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $email = $_POST['email']?? null;
-var_dump($email);
+// var_dump($email);
 
 include __DIR__ . '/utilities.php'
 
@@ -11,13 +11,21 @@ include __DIR__ . '/utilities.php'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Iscrizione-NewsLetter</title>
 </head>
-<body>
-    <form action="" method="post">
-        <input type="emial" name="email" id="email" placeholder="inserisci la mail">
-        <button>invia</button>
-    </form>
-    <h1><?php echo checkEmail($email)?></h1>
+<body style="background-color: #aaaaaa;">
+    <nav class="text-center pt-4">
+        <form  class="mb-3" action="" method="post">
+            <label class="d-block pb-1" for="email">Inserisci la tua mail: </label>
+            <input type="email" name="email" id="email" placeholder="inserisci la mail">
+            <button class="btn btn-primary">invia</button>
+        </form>
+
+    </nav>
+    <div class="container-fluid text-center">
+
+        <h1><?php echo checkEmail($email)?></h1>
+    </div>
 </body>
 </html>
--- utilities.php
+++ utilities.php
@@ -1,9 +1,19 @@
 <?php
 
-function checkEmail($email){
-    if(str_contains($email,'@') && str_contains($email, '.')){
-        echo 'your email is correct';
+function checkEmail($email)
+{
+    if($email === null){
+        echo 'insierisci la tua email';
     }else{
-        echo 'your email is not correct';
-    };
+        if (str_contains($email, '@') && str_contains($email, '.')) {
+            echo '<div class="alert alert-success" role="alert">
+            success !
+            </div>';
+        } else {
+            echo '<div class="alert alert-warning" role="alert">
+            warning alert—check it out you missed a . or @ !
+            </div>';
+        };
+
+    }
 }
