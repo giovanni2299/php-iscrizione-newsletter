c8c8813a5cdada6f7ac983c3e77e51059be70f3c
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-07 14:25:03 +0200

    ADD: created the document

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iscrizione-NewsLetter</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="emial" placeholder="inserisci la mail">
+    </form>
+</body>
+</html>
